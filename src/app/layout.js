1a0d4dc0d052dad0519710224b3da1a0a2cffbc6
Tomiwa <63702479+Tomiwa-S@users.noreply.github.com>
2024-07-05 13:21:22 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head';
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <Head>
+    {/* Not to be used in production */}
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />  
+    </Head>
       <body>{children}</body>
     </html>
   )
