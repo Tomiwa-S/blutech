e046c82d64d9c18282fd9cb03116b0cc0d4b4453
Tomiwa <63702479+Tomiwa-S@users.noreply.github.com>
2024-07-05 13:29:45 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,6 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Head from 'next/head';
 
-// const inter = Inter({ subsets: ['latin'] })
-
 export const metadata = {
   title: 'Unlimi',
   description: 'Blutech assignment',
@@ -12,10 +10,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <Head>
-    {/* Not to be used in production */}
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />  
-    </Head>
       <body>{children}</body>
     </html>
   )
